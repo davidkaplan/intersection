9a6e70dc28f683439d9368ff408fca49af5b7a53
David Kaplan <dkappy@gmail.com>
2025-02-05 20:30:56 -0800

    change how intersection works: breadth-first now and no longer recursive

--- intersection.cpp
+++ intersection.cpp
@@ -20,32 +20,54 @@ Intersection::Intersection(const Path& path, const Obstacle& obstacle) :
 {
 }
 
-bool Intersection::intersects(const Path& path, const Obstacle& obstacle) { 
+bool Intersection::intersects() const { 
     // if max height of obstacle is less than min height of path, return false
-    if (  obstacle.height < path.getMinHeight() )
+    if ( _obstacle.height < _path.getMinHeight() )
     {
+        std::cout << "obstacle height less than start of path, returning false." << std::endl;
         return false;
     }
 
     // truncate path by height of obstacle
-    Path truncated_path = path.truncateByHeight(obstacle.height);
-
-    // determine some reasonable precision, from the smallest bbox dimension of the obstacle, and the path radius
-
-
-    // recursively split path into segments, testing each one's bounding box agains the obstacle's bounding box
-    // if bounding boxes intersect, we test point internality with the cross product method
-    std::vector<Path> paths = {path};
+    Eigen::Vector2d orig_start_point = _path.getStartPoint();
+    Eigen::Vector2d orig_end_point = _path.getEndPoint();
+    Path truncated_path = _path.truncateByHeight(_obstacle.height);
 
     // test start and end point of original path for bounds... algorithm will only check midpoints
-    Eigen::Vector2d start_point = path.getStartPoint();
-    Eigen::Vector2d end_point = path.getEndPoint();
-    if ( obstacle.containsPoint(start_point) || obstacle.containsPoint(end_point) )
+    Eigen::Vector2d start_point = truncated_path.getStartPoint();
+    Eigen::Vector2d end_point = truncated_path.getEndPoint();
+    if ( _obstacle.containsPoint(start_point) ||  _obstacle.containsPoint(end_point) )
     {
+        std::cout << "Start or end point of path found inside polygon, returning true." << std::endl;
         return true;
     }
-    
 
-    return true; 
+    //
+    std::queue<Path> paths;
+    paths.push(truncated_path);
+    int iterations = 0;
+    while ( paths.size() > 0 && iterations < MAX_ITERATIONS )
+    {
+        iterations++;
+        Path path = paths.front();
+        if ( path.intersectsBBox( _obstacle.bbox) )
+        {
+            if ( _obstacle.containsPoint(path.getMidPoint()) )
+            {
+                return true;
+            }
+            std::vector<Path> split_path = path.split();
+            paths.push(split_path[0]);
+            paths.push(split_path[1]);
+            
+        }
+        paths.pop();
+    }
+    return false;
 }
 
+bool Intersection::intersects(const Path& path, const Obstacle& obstacle)
+{ 
+    Intersection i(path, obstacle);
+    return i.intersects();
+}
--- intersection.h
+++ intersection.h
@@ -5,7 +5,9 @@
 class Intersection {
     Path _path;
     Obstacle _obstacle;
+    const int MAX_ITERATIONS = 512;
 public: 
     Intersection(const Path& path, const Obstacle& obstacle);
+    bool intersects() const; 
     static bool intersects(const Path& path, const Obstacle& obstacle); 
 };
